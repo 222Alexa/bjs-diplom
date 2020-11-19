8cdb4c32871d36b61d30e264a3d5b6d944adbe57
Alexandra Titova <auto-steklo@mail.ru>
2020-11-20 01:01:27 +0300

    update

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,22 +1,23 @@
 'use strict';
 
-const newUser = new UserForm();
+const newUserForm = new UserForm();
 
-newUserForm. loginFormCallBack = function(data) =>{
-    ApiConnector.login((data, response) =>{
-        if(!response.success){
+newUserForm.loginFormCallback = data => {
+    ApiConnector.login(data, response => {
+        if (!response.success) {
             newUser.setLoginErrorMessage(response.error);
-        }else {
+        }
+        else {
             location.reload();
         }
     });
 }
-
-newUserForm.registerFormCallback = function (data) => {
-    ApiConnector.register((data, response) =>{
-        if(!response.success){
+newUserForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => {
+        if (!response.success) {
             newUser.setRegisterErrorMessage(response.error);
-        }else {
+        }
+        else {
             location.reload();
         }
     });
